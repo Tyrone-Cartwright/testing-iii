18ec90d7ebf0712f7bf3b291addefaad98ce764f
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-30 17:54:52 -0500

    Implemented display test and still debugging controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from "react";
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 import Controls from "./Controls";
@@ -40,3 +40,28 @@ describe("Control test", () => {
     getByText(/unlock gate/i);
   });
 });
+
+// Button Events
+// describe("Contols button tests", () => {
+//   it("renders open gate when close gate button clicked", () => {
+//     const { getByText } = render(<Controls />);
+
+//     const button = getByText(/close gate/i);
+
+//     fireEvent.click(button);
+
+//     getByText(/open gate/i);
+//   });
+// });
+
+// describe("Contols button tests", () => {
+//   it("renders unlocked when locked gate button clicked", () => {
+//     const { getByText } = render(<Controls />);
+
+//     const button = getByText(/lock gate/i);
+
+//     fireEvent.click(button);
+
+//     getByText(/open gate/i);
+//   });
+// });
